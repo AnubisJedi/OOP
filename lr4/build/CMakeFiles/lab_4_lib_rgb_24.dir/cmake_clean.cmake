41744b3b8ef26fa96c6662ed57372120494d2673
Max <sirmax389@gmail.com>
2024-12-02 12:00:41 +0000

    lr4

---
+++ lr4/build/CMakeFiles/lab_4_lib_rgb_24.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab_4_lib_rgb_24.dir/src/rgb_24.cpp.o"
+  "CMakeFiles/lab_4_lib_rgb_24.dir/src/rgb_24.cpp.o.d"
+  "liblab_4_lib_rgb_24.a"
+  "liblab_4_lib_rgb_24.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab_4_lib_rgb_24.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
